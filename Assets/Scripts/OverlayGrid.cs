afe8a4b9ede2a1c5c95f8fc977a7cfb649b3df6a
Halil Ibrahim Kayim <kayimhalil@hotmail.com>
2015-03-09 21:47:56 +0200

    started tile and grid overlaying

---
+++ Assets/Scripts/OverlayGrid.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverlayGrid : MonoBehaviour {
+
+    public int gridWidth;
+    public int gridHeight;
+    public float distanceBetweenTiles;
+
+    public GameObject tilePrefab;
+
+    void Awake()
+    {
+        this.drawGrid();
+    }
+
+    private void drawGrid()
+    {
+        for (int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                GameObject tile = Instantiate(tilePrefab,
+                    //assuming each tile has a size of 1x1
+                    new Vector3(-((gridWidth + ((gridWidth - 1) * distanceBetweenTiles)) / 2f) + i * (distanceBetweenTiles + 1),
+                    0.01f,
+                    -((gridHeight + ((gridHeight - 1) * distanceBetweenTiles)) / 2f) + j * (distanceBetweenTiles + 1)),
+                    tilePrefab.transform.rotation) as GameObject;
+                tile.transform.parent = transform;
+            }
+        }
+    }
+
+}
