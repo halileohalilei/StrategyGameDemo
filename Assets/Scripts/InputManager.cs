14caeda0fe53bcf86d48d43d80aaa083fdce35e7
Halil Ibrahim Kayim <kayimhalil@hotmail.com>
2015-03-16 22:33:11 +0200

    Revert "unnecessary scripts deleted"
    This reverts commit d6cae0d1eaca149c68d3736f9c0dc8ddd86ad698.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class InputManager : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                
+            }
+        }
+
+        public void OnScreenClick()
+        {
+            Event e = Event.current;
+            if (e.isMouse)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts
+{
+    public class Tile : MonoBehaviour {
+
+        public int I, J;
+
+        public int Width, Height;
+
+        public override string ToString()
+        {
+            return "Tile at position: " + I + ", " + J;
+        }
+
+        public void OnClick(BaseEventData data)
+        {
+            PointerEventData pointerData = data as PointerEventData;
+
+            if (pointerData != null) Debug.Log(pointerData.worldPosition);
+        }
+    }
+}
